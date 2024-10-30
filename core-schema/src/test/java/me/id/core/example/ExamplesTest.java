506a0236139c09f736474e0c58e999c3efba79c0
hal-idme <hal.hildebrand@id.me>
2024-10-30 09:44:50 -0700

    break out schemas into separate modules.  add -test module for test schema per module

---
+++ core-schema/src/test/java/me/id/core/example/ExamplesTest.java
@@ -0,0 +1,22 @@
+package me.id.core.example;
+
+import me.id.core.schema.Tables;
+import org.jooq.impl.DSL;
+import org.junit.Test;
+
+import java.sql.DriverManager;
+
+public class ExamplesTest {
+
+    private static String URL = "jdbc:postgresql://localhost:5432/idme_core";
+    private static String USER = System.getProperty("user.name");
+
+    @Test
+    public void simpleExample() throws Exception {
+        var conn = DriverManager.getConnection(URL, USER, "");
+        var context = DSL.using(conn);
+
+        var admins = context.selectFrom(Tables.COMMON_PASSWORDS).where(Tables.COMMON_PASSWORDS.ID.eq(33)).fetch();
+        System.out.println(admins);
+    }
+}
